97c907287e95a5d35905e5f25dacd94dadb634c1
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-09-18 21:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const line = document.getElementById("line")
+        let angle = 0
+
+        function rotate() {
+            // console.log(angle)
+            line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`
+            angle += 2
+        }
+
+        setInterval(rotate, 20)
